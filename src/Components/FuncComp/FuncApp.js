97235461818c73a1052ba0b5b166b866691e15ac
NeelimaSomishetty <neelimasomishetty2@gmail.com>
2023-06-02 00:56:00 +0530

    DAY3 Internal Styling to Functional Components

--- src/Components/FuncComp/FuncApp.js
+++ src/Components/FuncComp/FuncApp.js
@@ -2,10 +2,11 @@ import React from 'react';
 import FuncHeader from './FuncHeader';
 import "../ClassComp/ClassApp.css";
 // import "./FuncHeader.css";
-
+const myStyle={color:"green",fontFamily:"cursive",fontsize:"30px"}
 function FuncApp() {
     return (<div>
-        <h1 style={{ color: "blue", fontFamily: "serif", textAlign: "center", border: "5px solid ash",}}>functional app component</h1>
+        <h1 style={{ color: "blue", fontFamily: "serif", textAlign: "center", border: "5px solid ash", }}>functional app component</h1>
+        <h1 style={myStyle}>Hi Iam styled by Internal CSS</h1>
         <FuncHeader />
         </div>)
     
