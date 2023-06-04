fa1795c7304880a84e5e0e7bfa77ad1e381243ff
Maheswari2k1 <bmaheswari615@gmail.com>
2023-06-04 17:05:20 +0530

    Day-1 Sample web page

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    
+    <header class="h1"> 
+    <ul>
+      <li><a href="#">HOME</a></li>
+      <li><a href="#">Online Ticket Booking</a></li>
+      <li><a href="#">ABOUT</a></li>
+      <li><a href="#">HELP</a></li>
+    </ul>
+        <h1 class="heading">TIRUMALA</h1>
+        <p class="p">Tirumala is a spiritual town in Tirupati district of the Indian state of Andhra Pradesh. It is one of the suburbs of the Tirupati urban agglomeration. The town is a part of Tirupati Urban Development Authority and located in Tirupati (urban) mandal of Tirupati revenue division.[5] It is a hill town where Tirumala Venkateshvara Temple is located, a popular shrine of Vishnu. The town is strictly vegetarian.</p>
+      <br></br>
+      <p className="p">Tirumala is located 980 metres (3,200 ft) above sea level and covers an area of approximately 26.8 square kilometres (10.33 sq mi). Surrounding the hills are seven peaks of Seshachalam range, Eastern Ghats namely Seshadri, Neeladri, Garudadri, Anjanadri, Vrushabadri, Narayanadri, and Venkatadri. The temple of Venkateshvara[6] is on the seventh peak (Venkatadri).
+
+At the 21 km (13 mi) point on the Tirupati – Tirumala Ghat road, there is a major discontinuity of stratigraphic significance that represents a period of remarkable serenity in the geological history of the Earth. This is referred to as the Eparchaean Unconformity. This unconformity separates the Nagari Quartzite of the Proterozoic from the granite of the Archean, representing a time gap of 800 Mya.[7] In 2001, the Geological Survey of India (GSI) declared the Eparchaean Unconformity to be one of the 26 "Geological Monuments of India".</p>
+</header>
+    
+  );
+}
+
+export default App;
