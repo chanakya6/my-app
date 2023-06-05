cb2eceacc2b1e46ccb4d183d577773454115f3f2
deepasriyerramaneni <yerramanenideepasri@gmail.com>
2023-06-05 06:21:33 +0530

    initial changes

---
+++ src/components/FunctionalComp/FuncApp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function FuncApp(){
+    return <h1>Functional App Component</h1>
+}
+export default FuncApp;
---
+++ src/components/classComp/ClassApp.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'; 
+ import  './ClassApp.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class ClassApp extends Component{
+    render(){
+        return <h1>Class App Component</h1>
+    }
+}
+
+export default ClassApp;
