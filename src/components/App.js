387946d882472d36e9a5259761b4bea71c3222cc
sarithachitturi <sarithachitturi99@gmail.com>
2023-06-08 22:29:13 +0530

    initial changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
 import Phone from "./Navbar/Pages/Phone";
-import { Route } from "react-router-dom";
+// import { Route } from "react-router-dom";
 import Store from "./Navbar/Pages/Store";
 import TVSmartHome from "./Navbar/Pages/TVSmartHome";
 import LaptopTablet from "./Navbar/Pages/LaptopTablet";
+import PageNotFound from "./Navbar/Pages/PageNotFound";
 
 
 const App= () => {
@@ -17,6 +18,7 @@ const App= () => {
             <Route path='/Phone' element={<Phone/>} />
             <Route path='/TvSmartHome' element={<TVSmartHome/>} />
             <Route path='/LaptopTablet' element={<LaptopTablet/>} />
+            <Route path="*" element={<PageNotFound/>}/>
         </Routes>
         </div>
     )
