8aa1c634b170748e21d1817108c4e4ba7c72cc66
deepasriyerramaneni <yerramanenideepasri@gmail.com>
2023-06-09 06:18:22 +0530

    initial changes

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import "../NavBar/NavBar.css";
 
 const NavBar = () => {
   return (
     <div>
         <ul>
-            {/* <img src="https://m.media-amazon.com/images/I/41goJeEJx-L._SX300_SY300_QL70_FMwebp_.jpg" 
+            <img src="https://m.media-amazon.com/images/I/41goJeEJx-L._SX300_SY300_QL70_FMwebp_.jpg" 
             alt="America"
             height="50px"
             width="50px"
-/>   */}
+/>  
      <li>Home</li>
      <li>About</li>
      <li>Contactus</li>
