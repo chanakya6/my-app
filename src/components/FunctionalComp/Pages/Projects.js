7880b1053745cd8b1c3f01e82db7750ff6adfd1f
sheshugoudrangula <sheshugoudrangula@gmail.com>
2023-06-12 23:55:59 +0530

    table Task

--- src/components/FunctionalComp/Pages/Projects.js
+++ src/components/FunctionalComp/Pages/Projects.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react' ;
+import {Table} from "react-bootstrap";
+import axios from "axios";
 
 const Projects = () => {
+        const [result,setResult]=useState([]);
+        useEffect( ()=>{
+
+          getTodoData();
+
+        },[]);
+        const getTodoData = async ()=>{
+          let response = await axios.get("https://jsonplaceholder.typicode.com/todos")
+          setResult(response.data);
+        };
   return (
-    <div>
-     <h1>Projects</h1> 
-    </div>
+   <Table striped variant='dark' size='sm'>
+   <thead>
+      <tr>
+        <td>S.no</td>
+        <td>userId</td>
+        <td>Todo Title Name</td>
+        <td>isCompleted</td>
+      </tr>
+   </thead>
+   <tbody>
+    {result.length>0?(
+      result.map((eachTodo)=>(
+        <tr key = {eachTodo.id}>
+          <td>{eachTodo.id}</td>
+           <td>{eachTodo.userId}</td>
+            <td>{eachTodo.title}</td>
+             <td>{eachTodo.completed ? "yes" : "no"}</td>
+        </tr>
+      ))):(
+        <tr>
+          <td colSpan={4} style={{textAlign:"center"}}>
+           No Data Available
+          </td>
+        </tr>
+      )}
+   </tbody>
+   </Table>
   )
 }
 
