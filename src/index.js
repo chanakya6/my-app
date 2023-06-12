2ef32133f0133de2322a0f503f540ab115bbe9c9
NeelimaSomishetty <neelimasomishetty2@gmail.com>
2023-06-12 19:27:27 +0530

    DAY4 NumArrayList

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css"
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./Components/App";
 // import ClassApp from "./Components/ClassComp/ClassApp";
 // import ClassApp2 from "./Components/ClassComp2/ClassApp2";
 // import ClassApp3 from "./Components/ClassComp3/ClassApp";
 // import ClassApp4 from "./Components/ClassComp4/ClassApp4";
 // import ClassApp5 from "./Components/ClassComp5/ClassApp5";
-import FuncApp from "./Components/FuncComp/FuncApp";
+// import FuncApp from "./Components/FuncComp/FuncApp";
 // import FuncApp2 from "./Components/FuncApp2/FuncApp2";
 // import FuncApp3 from "./Components/FuncApp3/FuncApp3";
 // import FuncApp4 from "./Components/FuncApp4/FuncApp";
@@ -15,30 +16,22 @@ import FuncApp from "./Components/FuncComp/FuncApp";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import Button from 'react-bootstrap/Button';
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+// // <div>
+//     {/* <ClassApp />
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <div>
-        {/* <ClassApp />
-    
-        <ClassApp2 />
-        <ClassApp3 />
-        <ClassApp4 />
-        <ClassApp5 />
-        < button type="button" class="btn btn-primary" > Click me </button > */}
+//     <ClassApp2 />
+//     <ClassApp3 />
+//     <ClassApp4 />
+//     <ClassApp5 />
+//     < button type="button" class="btn btn-primary" > Click me </button > */}
 
-        <FuncApp />
-        {/* <FuncApp2 />
-        <Button variant="success">this is react-bootstrap button</Button>
-        <FuncApp3 />
-        <FuncApp4 />
-        <FuncApp5 />
-        <FuncApp5 /> */}
-    </div>
-
-   
-
-    
-);
-
-
+//     {/* <FuncApp /> */}
+//     {/* <FuncApp2 />
+//     <Button variant="success">this is react-bootstrap button</Button>
+//     <FuncApp3 />
+//     <FuncApp4 />
+//     <FuncApp5 />
+//     <FuncApp5 /> */}
+// // </div>
