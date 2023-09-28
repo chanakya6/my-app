b898ce79fdc1890a4fe1e8d50d0b2d0d38dbc220
sulochana-99 <rajasulochanasamineni99@gmail.com>
2023-09-28 23:35:28 +0530

    Created Caluclator App

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -2,21 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from 'react-router-dom';
-//import FoodApp from './FoodieApp/FoodApp';
-//import TodoApp from './Todolist/TodoApp';
-//import TodoApi from './Api/TodoApi';
-//import Todo from './Api/Todo';
-//import FunApp from './Components/functional-components/FunApp';
-//import FormEvents from './Api/FormEvents';
-import store from './Components/functional-components/Redux/Store';
-import { Provider } from 'react-redux';
-import ReduxApp from './Components/functional-components/NavBar/ReduxApp';
+import CalAppApp from "./CalculatorApp/CalApp"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}>
-   <BrowserRouter>  
-                <ReduxApp/>
-             </BrowserRouter>
-</Provider>);
-    
+root.render(
+    <div>
+        <CalAppApp/>
+    </div>
+);
